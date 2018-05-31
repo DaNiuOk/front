5e59869ae9e54a6cf223e92f2911cc5a1f99fb9e
冯欢 <fenghuan@mgzf.com>
2018-05-31 20:08:16 +0800

    webpack

---
+++ src/js/demo.js
@@ -0,0 +1,1 @@
+console.log('hello webpack312333555666');
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var fs = require('fs');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+
+module.exports = {
+  entry: {
+    demo: './src/js/demo.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].[chunkhash].js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000,
+    index: 'html/demo.html'
+  },
+  plugins: [
+    new UglifyJSPlugin(),
+    new CleanWebpackPlugin(
+      ['dist/js/*','dist/css/*'],
+      {
+        root: __dirname,
+        verbose: true,
+        dry: false
+      }
+    ),
+    new HtmlWebpackPlugin({
+      filename: 'html/demo.html',
+      template: path.resolve(__dirname, 'src/html/demo.html'),
+      inject  : 'body',
+      minify:{
+        collapseWhitespace: true,
+      }
+    })
+  ]
+}
